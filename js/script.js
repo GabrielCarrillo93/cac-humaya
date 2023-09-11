1195303cfb7045dd320a1bb144fed1b2e1bf4ccf
Gabriel Ivan Carrillo <gabrielcarrillo943@gmail.com>
2023-09-11 12:05:01 -0300

    cambio de api

--- js/script.js
+++ js/script.js
@@ -14,19 +14,26 @@ hamburguesa.addEventListener("click", () => {
 
 const cargarRecetas = async() => {
     try {
-        const respuesta = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=7161dca52b6649a787ff0f8bc79379e0&number=3&tags=dessert"`);
-        console.log(respuesta);
+        const url = 'https://tasty.p.rapidapi.com/recipes/list?from=0&size=3&q=caramel';
+        const options = {
+            method: 'GET',
+            headers: {
+                'X-RapidAPI-Key': 'db1b3d68a0mshc37fb74a634e89ep167a50jsna9d84d2dfb55',
+                'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
+            }
+        };
+
+        const respuesta = await fetch(url, options);
         
-        if (respuesta.status === 200){
+        if (respuesta.ok){
             const datos = await respuesta.json();
-            console.log(datos.recipes)
             let recetas = "";
-            datos.recipes.forEach(receta => {
+            datos.results.forEach(receta => {
                 recetas += `
                 <article class="receta">
-                    <img src="https://spoonacular.com/recipeImages/${receta.id}-480x360.${receta.imageType}" alt="${receta.title}" class="fotoReceta">
-                    <h3 class="tituloReceta">${receta.title}</h3>
-                    <p class="descripcionReceta">${receta.summary}</p>
+                    <img src="${receta.thumbnail_url}" class="fotoReceta" alt="${receta.name}">
+                    <h3 class="tituloReceta">${receta.name}</h3>
+                    <p class="descripcionReceta">${receta.description}</p>
                     <button class="btnReceta">Preparación</button>
                 </article>
                 `
@@ -45,3 +52,5 @@ const cargarRecetas = async() => {
 }
 
 cargarRecetas();
+
+
